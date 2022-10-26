ccb8443a22471acb2c1b87c6b3ceaae6f0f4970d
Siddique C M <siddique.cm@pitsolutions.com>
2022-10-26 09:25:11 +0530

    backend changes

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,16 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.get("/slug/:slug", (req, res) => {
+app.get("/api/slug/:slug", (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found"});
+  }
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
